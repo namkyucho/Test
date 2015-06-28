5bd98bc1daea3b256511b11b8aeb9731ad0695e3
Administrator <admin@local.host>
2015-06-28 02:06:17 -0700

    Auth Test 1

---
+++ AuthTokenEST/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace AuthTokenEST.Controllers
+{
+    public class BaseController : Controller
+    {
+        public class CheckTokenFilter : ActionFilterAttribute
+        {
+            public override void OnActionExecuting(ActionExecutingContext filterContext)
+            {
+                filterContext.Controller.ViewBag.TestMsg = "Custom Action Filter: Message from OnActionExecuting Method";
+                var D1 = filterContext.HttpContext.Request.QueryString["d1"];
+                if (D1 != null)
+                { 
+                    filterContext.Controller.ViewBag.Request_1 = D1; 
+                }
+            }
+        }
+        
+    }
+}
---
+++ AuthTokenEST/Controllers/MainController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthTokenEST.Controllers
+{
+    public class MainController : BaseController
+    {
+        //
+        // GET: /Main/
+        [CheckTokenFilter]
+        public ActionResult Index(decimal d1, decimal d2)
+        {
+            return View();
+        }
+
+    }
+}
